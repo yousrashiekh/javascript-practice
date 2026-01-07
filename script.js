0ceafd643d148e5e356402a6cdcb167917a3dd51
yousra shiekh <yousrashiekh043@gmail.com>
2026-01-07 19:44:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// String variables
+let firstName = "Yusra";
+console.log("First Name:", firstName);
+
+let lastName = "Khan";
+console.log("Last Name:", lastName);
+
+let city = "Karachi";
+console.log("City:", city);
+
+let country = "Pakistan";
+console.log("Country:", country);
+
+// Number variables
+let age = 18;
+console.log("Age:", age);
+
+let rollNumber = 123;
+console.log("Roll Number:", rollNumber);
+
+let marks = 85;
+console.log("Marks:", marks);
+
+let totalMarks = 100;
+console.log("Total Marks:", totalMarks);
+
+// Boolean variables
+let isStudent = true;
+console.log("Is Student:", isStudent);
+
+
+
+// Undefined & Null
+let futurePlan;
+console.log("Future Plan:", futurePlan);
+
+let result = null;
+console.log("Result:", result);
+
+// Constant variable
+const institute = "bano qabil";
+console.log("Institute:", institute);
+
+let num1 = 10;
+console.log("Num1:", num1);
+
+let num2 = 5;
+console.log("Num2:", num2);
+
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+let subtract = num1 - num2;
+console.log("Subtract:", subtract);
+
+let multiply = num1 * num2;
+console.log("Multiply:", multiply);
