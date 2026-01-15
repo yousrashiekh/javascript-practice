ce90856f56735bc7e89bde2268be7473e9f9e7d5
Yusra saeed <yousrashiekh043@gmail.com>
2026-01-15 20:26:11 +0500

    this is while loop js

---
+++ while loop.js
@@ -0,0 +1,49 @@
+let i1 = 10;
+while(i1>=1){
+     console.log(i1)
+    i1--;
+}
+
+let num = 1;
+while(num<=15){
+    if(num%2!==0){
+        console.log(num)
+    }
+     num++;
+}
+
+let sum = 0
+let numb = 0
+while(numb<=100){
+    if(numb%2===0){
+        console.log("even number:",numb)
+        sum+=numb;
+    }
+    numb++;
+}
+console.log("sum",sum);
+
+let gamenum = 10
+let username = ( "guess the game number:" );
+while(username!=gamenum){
+        username = ( "guess the right number ");
+}
+
+console.log( "congratulation,you entered the right number")
+
+
+
+let a = 1;
+    while( a < 10){
+        console.log( a);
+        a++;
+    }
+
+    let b =1;
+    while(b<=15){
+        if(b%2!==0){
+            console.log("old Number:" , b)
+        } 
+        b++;
+        }
+        
